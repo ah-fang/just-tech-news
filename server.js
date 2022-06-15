656a596deee99aa275dede380d0736945ee63b3c
Andrea Fang <arhughes43@gmail.com>
2022-06-15 15:44:18 -0700

    logout functionality

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path'); 
+const session = require('express-session');
 const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const hbs = exphbs.create({});
 const app = express();
@@ -14,6 +26,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
